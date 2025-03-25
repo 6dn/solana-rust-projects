af626a0f61a3c7bb0c16cb9acc801ab1c1f159e9
xdngo <xdngo@qq.com>
2025-03-25 20:36:46 +0800

    modify

--- my_solana_program/src/lib.rs
+++ my_solana_program/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    program::invoke_signed,
+    system_instruction,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+    let from_account = next_account_info(account_info_iter)?;
+    let to_account = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    let transfer_instruction = system_instruction::transfer(
+        from_account.key,
+        to_account.key,
+        1000000, // lamports
+    );
+
+    invoke_signed(
+        &transfer_instruction,
+        &[from_account.clone(), to_account.clone(), system_program.clone()],
+        &[],
+    )?;
+
+    Ok(())
 }
